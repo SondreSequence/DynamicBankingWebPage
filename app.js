4db3a2f68aa44705b5712e17f2885197a48650df
SondreSequence <sondre.kristoffer.reinholdtsen@no.experis.com>
2023-01-17 19:27:36 +0100

    Detailed commenst and removed console.logs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Buttons and tags we need to update or modify when functions are called
 const loanButton = document.getElementById("loanbutton");
 const workButton = document.getElementById("workbutton");
 const bankButton = document.getElementById("bankbutton");
@@ -13,20 +14,21 @@ const priceElement = document.getElementById("price");
 const select = document.getElementById("selectlist");
 const pcInfoText = document.getElementById("pcInfoText");
 
-//Add event listeners to buttons so that when clicked, the corresponding functions are called
+//Event listeners to buttons so that when clicked, the corresponding functions are called
 loanButton.addEventListener("click", addLoan);
 workButton.addEventListener("click", addWork);
 bankButton.addEventListener("click", storeMoney);
 repayButton.addEventListener("click", repayLoan);
 buyButton.addEventListener("click", buyPc);
 
-// Initialize global variables to keep track of the loan, work, bank and id of the selected index from select.
+// Global variables to keep track of the loan, work, bank and id of the selected index from select
 let loan = 0;
 let work = 0;
 let bank = 0;
 let selectedID = 0;
 let workClickCounter = 0;
 
+//An async function which gets data asynchronously and returns the response in a json format.
 async function getComputersData() {
   const response = await fetch(
     "https://hickory-quilled-actress.glitch.me/computers",
@@ -40,6 +42,8 @@ async function getComputersData() {
   return await response.json();
 }
 
+/*Function to add loan if you don't ask for more than double of that in your work account. 
+Whilst also making sure you can't loan if you already have one that needs to be repaid*/
 function addLoan() {
   let pay = parseInt(payElement.innerText.split(":")[1]);
   let wantedLoan = prompt("Amount: ");
@@ -57,6 +61,7 @@ function addLoan() {
   }
 }
 
+// Function to buy a pc if you have enough money in your bank, and it also updates the image of a sad Donald to a happy one when the pc is bought :D.
 async function buyPc() {
   const pc = await getComputersData();
   const pcprice = pc[selectedID].price;
@@ -74,6 +79,9 @@ async function buyPc() {
   }
 }
 
+/*This function does as the name suggests aka repays your loan if you have enough in you work account
+ and transfers the rest to the bank if you have money left after the loan is repaid.
+ It also updates variables and tags accordingly*/
 function repayLoan() {
   if (work - loan >= 0) {
     payElement.innerText = "Pay: " + (work - loan);
@@ -85,10 +93,12 @@ function repayLoan() {
     balanceElement.innerText = "Balance: " + bank + " kr";
     repayButton.style.display = "none";
   } else {
-    alert("Work more to be eligible for a loan!");
+    alert("Work more to repay your loan!");
   }
 }
 
+/*This function stores money to the bank whilst also making sure 10% is deducted from the work amount if you have yet to repay your loan.
+It also updates variables and tags accordingly*/
 function storeMoney() {
   let deducted = work / 10;
   if (loan > 0) {
@@ -111,15 +121,17 @@ function storeMoney() {
   balanceElement.innerText = "Balance: " + bank + " kr";
 }
 
+//This function adds 100kr everytime the function is called.
 function addWork() {
   work += 100;
   payElement.innerText = "Pay : " + work + " kr";
-  // Since the meme love is real :P
+  //Had to include a funny haha meme since the meme love is real. It would be a shame if you never clicked the work button 3 times.
   workClickCounter++;
   if (workClickCounter == 3)
     window.open("https://youtu.be/dQw4w9WgXcQ", "_blank");
 }
 
+//An async function which gets the pc data asynchronously and iterates through each one to populate the select with the pc titles.
 async function addPcs() {
   const pcs = await getComputersData();
   for (let i = 0; i < pcs.length; i++) {
@@ -131,7 +143,6 @@ async function addPcs() {
 
 const populateSelect = (pc) => {
   const selectElement = document.getElementById("selectlist");
-  console.log(document.getElementById("loanbutton").innerText);
   const option = document.createElement("option");
   option.value = pc.price;
   option.text = pc.title;
@@ -139,6 +150,8 @@ const populateSelect = (pc) => {
   selectElement.appendChild(option);
 };
 
+/*Populates the selected option of the select and other elements that need data from the database and then parses the description with .replace()
+ for a cleaner look. */
 async function populateSelectedOption(id) {
   const pc = await getComputersData();
   let output = JSON.stringify(pc[id].specs)
@@ -148,6 +161,8 @@ async function populateSelectedOption(id) {
   descElement.innerText = pc[id].description;
   titleElement.innerText = pc[id].title;
   priceElement.innerText = "Price: " + pc[id].price + " kr";
+
+  //Replaces the images .jpg with .png if the image doesn't load.
   imgElement.src = "https://hickory-quilled-actress.glitch.me/" + pc[id].image;
   imgElement.onerror = function () {
     imgElement.src =
@@ -156,16 +171,18 @@ async function populateSelectedOption(id) {
   };
 }
 
-// Eventlistener to grab the right data according to what is selected
+/*Eventlistener to grab the right data according to what is selected.
+SelectID is selectedOptions.id-1 so it can match the json id's numbering. 
+Then we call the populatedSelectionOption which does what the name suggests using selectedID;
+*/
 select.addEventListener("change", (event) => {
   const selectedIndex = event.target.selectedIndex;
   const options = event.target.options;
   const selectedOption = options[selectedIndex];
-  selectedID = selectedOption.id - 1;
-  console.log(selectedID);
+  selectedID = selectedOption.id;
   populateSelectedOption(selectedID);
 });
 
-// Function calls on page load
+// Function calls on page load to grab the pc data and then populate the select, desc and titles with information about the first pc in the array aka location 0;
 addPcs();
 populateSelectedOption(0);
